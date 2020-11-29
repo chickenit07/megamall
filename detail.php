4cada4653f706460b28e0da7e93f8fb556607928
Chris Pham <chris@trueplus.vn>
2020-11-30 02:40:22 +0700

    init

---
+++ detail.php
@@ -0,0 +1,43 @@
+<?php
+include("controllers/PageController.php");
+$pageControl = new PageController();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php  require_once('views/user/blocks/head.html'); ?>
+</head>
+<body>
+    <!-- Header Start -->
+    <header>
+      <?php include("views/user/blocks/header.php"); ?>
+      <div class="container">
+        <?php $pageControl->viewNavigation(); ?>
+    </div>
+</header>
+<!-- Header End -->
+
+<div id="maincontainer">
+<?php
+//<!-- Slider End-->
+
+//<!-- Section Start-->
+include('views/user/blocks/otherdetail.php');
+//<!-- Section End-->
+
+//@yield('content')
+$pageControl->viewProductDetailPage();
+?>
+</div>
+<?php
+    //<!-- Footer -->
+include('views/user/blocks/footer.html');
+
+    //<!-- javascript
+       // ================================================== -->
+    //<!-- Placed at the end of the document so the pages load faster -->
+include('views/user/blocks/scripts.html');
+?>
+</body>
+</html>
